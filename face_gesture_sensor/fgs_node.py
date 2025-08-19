8fd8a7433189db8746122f985905bcf392751f52
Sergei Grichine <msghub@hotmail.com>
2025-08-19 10:36:11 -0500

    Update fgs_node.py

--- face_gesture_sensor/fgs_node.py
+++ face_gesture_sensor/fgs_node.py
@@ -195,7 +195,8 @@ def main(args=None):
         node.get_logger().info("Keyboard interrupt received. Shutting down node.")
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
