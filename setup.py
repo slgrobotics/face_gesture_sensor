9ad0b0534b7dbf4c66db97d32fe60f18f870c731
Sergei Grichine <msghub@hotmail.com>
2025-12-15 18:44:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         # launch files
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # config files
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=[
         'setuptools',
